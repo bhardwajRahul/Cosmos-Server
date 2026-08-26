c05b63b73ec13092b3662bb379a062d9525f4f2e
Yann Stepienik <azukaar@gmail.com>
2026-08-26 18:36:46 +0100

    [release] v0.23.0

--- src/index.go
+++ src/index.go
@@ -206,7 +206,7 @@ func main() {
 }
 
 // @title Cosmos Server API
-// @version 0.23.0-unstable013
+// @version 0.23.0
 // @description REST API for Cosmos Cloud server management
 // @BasePath /cosmos
 // @securityDefinitions.apikey BearerAuth
